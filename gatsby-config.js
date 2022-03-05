f8749bd59c5d2c5356a6e0525a8eb4acff920ce9
towa0518 <sakasyu.10.22@outlook.com>
2022-03-05 11:57:31 +0900

    fix サイトのヘッダーの背景色をサックスブルーに、サイトのタイトルを`Summary of Jubilo Blog`に変更

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Summary of Jubilo Blog`,
+    description: `This site is a summary of Jubilo Iwata Blog`,
+    author: `Towa`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import { Link } from "gatsby"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `#8bacd9`,
       marginBottom: `1.45rem`,
     }}
   >
